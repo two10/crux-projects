59e63ef80156ab33b51d6201a8864436a4feca7f
samuel <samuel@cruxframework.org>
2014-04-28 17:58:59 +0000

    First commit;

---
+++ smart-gwt-showcase/src/main/java/org/cruxframework/smartgwtshowcase/client/controller/samples/tree/TreeController.java
@@ -0,0 +1,9 @@
+package org.cruxframework.smartgwtshowcase.client.controller.samples.tree;
+
+import org.cruxframework.crux.core.client.controller.Controller;
+
+@Controller("treeController")
+public class TreeController 
+{
+	
+}
