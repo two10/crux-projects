409b71f0343d6aeb65ad5b26a132508a9962a5bb
wesley@triggolabs.com <wesley@triggolabs.com@621a468c-26a1-3c2b-13af-01ffc1995c69>
2014-05-06 18:27:50 +0000

    Added DeviceAdaptiveGrid Sample

--- trunk/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/ShowcaseMessages.java
+++ trunk/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/ShowcaseMessages.java
@@ -4,6 +4,8 @@
 package org.cruxframework.crossdeviceshowcase.client;
 
 import com.google.gwt.i18n.client.Messages;
+import com.google.gwt.i18n.client.Messages.DefaultMessage;
+
 
 /**
  * @author Thiago da Rosa de Bustamante
