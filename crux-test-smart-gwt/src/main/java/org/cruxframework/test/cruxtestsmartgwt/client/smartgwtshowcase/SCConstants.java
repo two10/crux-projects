374b2b2c3791f112aceff53434a6de019017979f
alexandre <alexandre@triggolabs.com>
2014-05-16 18:16:03 +0000

    IGNORE - Creating branch 5.1

--- crux-test-smart-gwt/src/main/java/org/cruxframework/test/cruxtestsmartgwt/client/smartgwtshowcase/SCConstants.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.test.cruxtestsmartgwt.client.smartgwtshowcase;
-
-import com.google.gwt.i18n.client.Constants;
-
-public interface SCConstants extends Constants {
-	@DefaultBooleanValue(false)
-	boolean websiteMode();
-}
