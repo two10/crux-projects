aa8ea63b351d738673f8e97a86b7619f0e86a693
claudio.junior@cruxframework.org <claudio.junior@cruxframework.org@621a468c-26a1-3c2b-13af-01ffc1995c69>
2014-05-16 20:09:28 +0000

    changes to storyboard and prommobanner (storyboard isn't ready yet, the declarative items should be properly passed to the controller again)

--- trunk/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/storyboard/StoryboardController.java
+++ trunk/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/storyboard/StoryboardController.java
@@ -29,20 +29,21 @@ public class StoryboardController
 
 	public void loadItens()
 	{
-		List<StoryboardItem> listItem = new ArrayList<StoryboardItem>();
-
-		new StoryboardItem("img/celular1.jpg", "Smartphone Dual Chip Desbloqueado Preto Android Kit Kat", "Por: R$ 899,00", "Comprar",listItem);
-		new StoryboardItem("img/laptop.jpg", "Notebook Ultrafino com Intel Core i3 2GB 500GB LED 14\" Preto", "Por: R$ 1.449,00", "Comprar",listItem);
-		new StoryboardItem("img/celular2.jpg", "Smartphone Desbloqueado Preto Android 4.2.2", "Por: R$ 999,90", "Comprar",listItem);
-		new StoryboardItem("img/tv1.jpg", "Smart TV LED 42\" HD com Conversor Digital 3 HDMI 3 USB", "Por: R$ 1.469,90", "Comprar",listItem);
-		new StoryboardItem("img/lavadora.jpg", "Lavadora de Roupas - 11Kg - Branca", "Por: R$ 1.209,90", "Comprar",listItem);
-		new StoryboardItem("img/tablet.jpg", "Tablet Galaxy Tab 3 T110N Lite Android 4.2 Tela", "Por: R$ 859,90", "Comprar",listItem);
-		new StoryboardItem("img/tv2.jpg", "TV LED 14\" HD com Conversor Digital, HDMI, USB", "Por: R$ 599,90", "Comprar",listItem);
-		new StoryboardItem("img/celular3.jpg", "Smartphone Desbloqueado Android 4.2.2", "Por: R$ 699,90", "Comprar",listItem);
-		
-		for(StoryboardItem w : listItem){
-			storyboardview.storyboard().add(w);
-		}
+//		List<StoryboardItem> listItem = new ArrayList<StoryboardItem>();
+//
+//		new StoryboardItem(url, descricao, preco, textButton, listItem)
+//		new StoryboardItem("img/celular1.jpg", "Smartphone Dual Chip Desbloqueado Preto Android Kit Kat", "Por: R$ 899,00", "Comprar",listItem);
+//		new StoryboardItem("img/laptop.jpg", "Notebook Ultrafino com Intel Core i3 2GB 500GB LED 14\" Preto", "Por: R$ 1.449,00", "Comprar",listItem);
+//		new StoryboardItem("img/celular2.jpg", "Smartphone Desbloqueado Preto Android 4.2.2", "Por: R$ 999,90", "Comprar",listItem);
+//		new StoryboardItem("img/tv1.jpg", "Smart TV LED 42\" HD com Conversor Digital 3 HDMI 3 USB", "Por: R$ 1.469,90", "Comprar",listItem);
+//		new StoryboardItem("img/lavadora.jpg", "Lavadora de Roupas - 11Kg - Branca", "Por: R$ 1.209,90", "Comprar",listItem);
+//		new StoryboardItem("img/tablet.jpg", "Tablet Galaxy Tab 3 T110N Lite Android 4.2 Tela", "Por: R$ 859,90", "Comprar",listItem);
+//		new StoryboardItem("img/tv2.jpg", "TV LED 14\" HD com Conversor Digital, HDMI, USB", "Por: R$ 599,90", "Comprar",listItem);
+//		new StoryboardItem("img/celular3.jpg", "Smartphone Desbloqueado Android 4.2.2", "Por: R$ 699,90", "Comprar",listItem);
+//		
+//		for(StoryboardItem w : listItem){
+//			storyboardview.storyboard().add(w);
+//		}
 	}
 
 	@BindView("storyBoard")
