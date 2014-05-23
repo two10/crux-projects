4bfab72a18651f1c721f18e6255f200e619a1542
samuel <samuel@cruxframework.org>
2014-05-23 15:10:20 +0000

    IGNORE adding rest call

---
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/remote/MyRestProxy.java
@@ -0,0 +1,12 @@
+package org.cruxframework.crossdeviceshowcase.client.remote;
+
+import org.cruxframework.crossdeviceshowcase.shared.dto.MyDTO;
+import org.cruxframework.crux.core.client.rest.Callback;
+import org.cruxframework.crux.core.client.rest.RestProxy;
+import org.cruxframework.crux.core.client.rest.RestProxy.TargetRestService;
+
+@TargetRestService("myRestService")
+public interface MyRestProxy extends RestProxy
+{
+	void myRestCall(String param1, int param2, Callback<MyDTO> callback);
+}
