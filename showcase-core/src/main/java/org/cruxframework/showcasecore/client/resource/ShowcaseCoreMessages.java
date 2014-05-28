331d500b6b8f70c81b39606af2aaeaac926130f1
samuel <samuel@cruxframework.org>
2014-05-28 22:56:51 +0000

    IGNORE adding more content to showcase

--- showcase-core/src/main/java/org/cruxframework/showcasecore/client/resource/ShowcaseCoreMessages.java
+++ showcase-core/src/main/java/org/cruxframework/showcasecore/client/resource/ShowcaseCoreMessages.java
@@ -1,7 +1,9 @@
 /**
  * 
  */
-package org.cruxframework.showcasecore.client;
+package org.cruxframework.showcasecore.client.resource;
+
+import org.cruxframework.crux.core.client.i18n.MessageName;
 
 import com.google.gwt.i18n.client.Messages;
 
@@ -9,7 +11,8 @@ import com.google.gwt.i18n.client.Messages;
  * @author Thiago da Rosa de Bustamante
  *
  */
-public interface ShowcaseMessages extends Messages
+@MessageName("messages_core")
+public interface ShowcaseCoreMessages extends Messages
 {
 	@DefaultMessage("O seu navegador não suporta a criação de bancos de dados locais.")
 	String databaseNotSupportedError();
