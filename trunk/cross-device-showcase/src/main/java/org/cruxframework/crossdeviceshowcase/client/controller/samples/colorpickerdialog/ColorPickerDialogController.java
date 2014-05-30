243ececd80660fa8712ebfc503480d86478f23c6
flavia@triggolabs.com <flavia@triggolabs.com@621a468c-26a1-3c2b-13af-01ffc1995c69>
2014-05-30 15:13:01 +0000

    IGNORE modified files and widgets internationalization

--- trunk/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/colorpickerdialog/ColorPickerDialogController.java
+++ trunk/cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/colorpickerdialog/ColorPickerDialogController.java
@@ -5,9 +5,11 @@ import org.cruxframework.crux.core.client.controller.Expose;
 import org.cruxframework.crux.core.client.ioc.Inject;
 import org.cruxframework.crux.core.client.screen.views.BindView;
 import org.cruxframework.crux.core.client.screen.views.WidgetAccessor;
-import org.cruxframework.crux.widgets.client.button.Button;
 import org.cruxframework.crux.widgets.client.colorpicker.ColorPickerDialog;
 
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Label;
 
 @Controller("colorPickerDialogController")
@@ -15,56 +17,53 @@ public class ColorPickerDialogController
 {
 	@Inject
 	private ColorPickerView colorpickerview;
-	
+
+	@Inject
+	private ColorPickerDialogMessages messages;
+
 	@Expose
-	public void onLoad()
+	public void showColorPicker()
 	{
-		//TODO finish this implementation
+		colorpickerview.colorPicker().show();
+		colorpickerview.colorPicker().setPopupPosition(700, 200);
+		close();
 	}	
-	
+
+	private void close()
+	{
+		History.addValueChangeHandler(new ValueChangeHandler<String>()
+				{  
+			public void onValueChange(ValueChangeEvent<String> event)
+			{
+				colorpickerview.colorPicker().hide();
+			}
+				});
+	}
+
 	@Expose
 	public void changeColor()
 	{
-		if (colorpickerview.colorPickerDialog().getColor() != null)
+		if (colorpickerview.colorPicker().getColor() != null || colorpickerview.colorPicker().getColor().trim().equals(""))
 		{
-			colorpickerview.labelText().getElement().getStyle().setColor("#"+colorpickerview.colorPickerDialog().getColor());
-			colorpickerview.labelColor().setText("Choosed color: #"+colorpickerview.colorPickerDialog().getColor());
+			colorpickerview.labelClickButton().getElement().getStyle().setColor("#"+colorpickerview.colorPicker().getColor());
+			colorpickerview.labelColorValue().setText(messages.labelColorValue()+" #"+colorpickerview.colorPicker().getColor());
 		}
 	}
-	
-	@Expose
-	public void callColorPicker()
-	{
-		colorpickerview.colorPickerDialog().show();
-		colorpickerview.colorPickerDialog().setPopupPosition(700, 200);
-	}	
-//	1) Ele tem um bug que, ao clicarmos sobre ele e depois clicarmos no botão de back ou forward do navegador, a janela dele continua aberta. Isto é um bug, mas para contornar este problema por enquanto, faça:
-//		final ColorPickerDialog picker = screen.corPicker();
-//		        History.addValueChangeHandler(new ValueChangeHandler<String>()
-//		        {
-//		            
-//		            @Override
-//		            public void onValueChange(ValueChangeEvent<String> event)
-//		            {
-//		                picker.hide();
-//		            }
-//		        });
-//
-//		2) Para diferenciar o clique no botão "Cancelar" de "OK" quando você abre um colorPicker, veja a propriedade "screen.corPicker().getColor()". Se ela vier vazia, então o usuário clicou no botão "Cancelar". Achei esse contrato terrível, por isso estou adicionando o Thiago na conversa para ver a opinião dele.
-//
-//		Att,
 
 	@BindView("colorPickerDialog")
 	public static interface ColorPickerView extends WidgetAccessor
 	{
-		ColorPickerDialog colorPickerDialog();
-		Button button();
-		Label labelText();
-		Label labelColor();
+		ColorPickerDialog colorPicker();
+		Label labelClickButton();
+		Label labelColorValue();
 	}
 
 	public void setColorpickerview(ColorPickerView colorpickerview) 
 	{
 		this.colorpickerview = colorpickerview;
+	}
+
+	public void setMessages(ColorPickerDialogMessages messages) {
+		this.messages = messages;
 	}    
 }
