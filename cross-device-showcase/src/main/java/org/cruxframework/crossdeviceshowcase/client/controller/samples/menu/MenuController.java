6fa2b07eda83a5a2227e53be47c25c14cf7fe02c
flavia <flavia@triggolabs.com>
2014-06-04 15:27:19 +0000

    IGNORE fixing menu test

--- cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/menu/MenuController.java
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/menu/MenuController.java
@@ -5,10 +5,6 @@ import org.cruxframework.crux.core.client.controller.Expose;
 import org.cruxframework.crux.core.client.ioc.Inject;
 import org.cruxframework.crux.core.client.screen.views.BindView;
 import org.cruxframework.crux.core.client.screen.views.WidgetAccessor;
-import org.cruxframework.crux.smartfaces.client.button.Button;
-import org.cruxframework.crux.smartfaces.client.menu.Menu;
-import org.cruxframework.crux.smartfaces.client.menu.MenuWidget.Orientation;
-import org.cruxframework.crux.smartfaces.client.menu.MenuWidget.Type;
 
 import com.google.gwt.user.client.ui.FlowPanel;
 
@@ -21,17 +17,17 @@ public class MenuController
 	@Expose
 	public void onLoad()
 	{
-		Menu menu = new Menu(Orientation.HORIZONTAL, Type.TREE);
-		
-		Button w1 = new Button();
-		w1.setText("AAAAA");
-		Button w2 = new Button();
-		w2.setText("BBBBB");
-		
-		menu.add(w1);
-		menu.add(w2);
-		
-		view.rootPanel().add(menu);
+//		Menu menu = new Menu(Orientation.HORIZONTAL, Type.TREE);
+//		
+//		Button w1 = new Button();
+//		w1.setText("AAAAA");
+//		Button w2 = new Button();
+//		w2.setText("BBBBB");
+//		
+//		menu.add(w1);
+//		menu.add(w2);
+//		
+//		view.rootPanel().add(menu);
 	}
 	
 	@BindView("menu")
