9dd706c6444d34f53e6b1bceed97d68867e74b52
guilherme <guilherme@triggolabs.com>
2014-06-04 15:32:04 +0000

    update in the methods enviarData(), enviarPessoa();

--- crux-test-widgets/src/main/java/org/cruxframework/test/cruxtestwidgets/server/MyRestService.java
+++ crux-test-widgets/src/main/java/org/cruxframework/test/cruxtestwidgets/server/MyRestService.java
@@ -2,10 +2,15 @@ package org.cruxframework.test.cruxtestwidgets.server;
 
 
 
+import java.util.Date;
+
 import org.cruxframework.crux.core.server.rest.annotation.RestService;
 import org.cruxframework.crux.core.shared.rest.annotation.GET;
 import org.cruxframework.crux.core.shared.rest.annotation.Path;
 import org.cruxframework.crux.core.shared.rest.annotation.PathParam;
+import org.cruxframework.crux.core.shared.rest.annotation.QueryParam;
+import org.cruxframework.test.cruxtestwidgets.client.modelo.Pessoa;
+
 
 @RestService("myRestService")
 @Path("rest")
@@ -15,27 +20,37 @@ public class MyRestService {
 	@GET
 	@Path("inteiro/{numero}")
    public Integer enviarInteiro(@PathParam("numero") Integer numero){    	
-		return numero + 3;
+		return numero;
     }
 	
 	@GET
 	@Path("palavra/{palavra}")
     public String enviarString(@PathParam("palavra")String palavra){    
-		return "Você enviou a palavra: " + palavra; 
+		return palavra; 
     }
 	
 	@GET
 	@Path("double/{numero}")
 	public Double enviarDouble(@PathParam("numero")Double numero){		
-		return numero/3;
+		return numero;
+	}
+		
+	@GET
+	@Path("caracter/{caracter}")
+	public Character enviarChar(@PathParam("caracter")Character caracter){		
+		return caracter;
 	}
 	
+	@GET
+	@Path("pessoa/{pessoa}")
+	public Pessoa enviarPessoa(@QueryParam("pessoa")Pessoa pessoa){		
+		return pessoa;
+	}
 	
 	@GET
-	@Path("caracter/{char}")
-	public Character enviarChar(@PathParam("char")Character caracter){		
-		
-		return caracter;
+	@Path("data/{data}")
+	public Date enviarData(@PathParam("data")Date data){		
+		return data;
 	}
 
 }
