b307d441594dad6740c7cbfa2b17772b5cde6b35
samuel <samuel@cruxframework.org>
2014-06-11 16:53:39 +0000

    IGNORE removing wrong class commited

--- cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/cruxbutton/CruxButtonController.java
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/cruxbutton/CruxButtonController.java
@@ -1,6 +1,5 @@
 package org.cruxframework.crossdeviceshowcase.client.controller.samples.cruxbutton;
 
-import org.cruxframework.crossdeviceshowcase.client.util.StatesController;
 import org.cruxframework.crux.core.client.controller.Controller;
 import org.cruxframework.crux.core.client.controller.Expose;
 import org.cruxframework.crux.core.client.screen.views.View;
@@ -8,14 +7,13 @@ import org.cruxframework.crux.core.client.screen.views.View;
 import com.google.gwt.user.client.ui.Widget;
 
 @Controller("cruxButtonController")
-public class CruxButtonController extends StatesController
+public class CruxButtonController
 {
 
 	private Widget widget = View.of(CruxButtonController.this).getWidget("cruxBtn");
 	
 	private void handleStates(String state) throws Exception
 	{
-		StatesController.changeWidgetState(widget, state);
 	}
 	
 	@Expose
