0f97e0fb470b1ca4afc5fb064d9cb71424dfaa85
samuel <samuel@cruxframework.org>
2014-06-11 18:33:40 +0000

    IGNORE changing worng class

--- cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/cruxbutton/CruxButtonController.java
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/cruxbutton/CruxButtonController.java
@@ -9,33 +9,32 @@ import com.google.gwt.user.client.ui.Widget;
 @Controller("cruxButtonController")
 public class CruxButtonController
 {
-
-	private Widget widget = View.of(CruxButtonController.this).getWidget("cruxBtn");
-	
-	private void handleStates(String state) throws Exception
+	private void handleStates(String state)
 	{
+		Widget widget = View.of(CruxButtonController.this).getWidget("cruxBtn");
+		widget.setStyleName(state);
 	}
 	
 	@Expose
-	public void setStateDefault() throws Exception
+	public void handleDefault()
 	{	
 		handleStates("default");
 	}
 
 	@Expose
-	public void setStateSuccess() throws Exception
+	public void handleSuccess()
 	{
 		handleStates("success");
 	}
 
 	@Expose
-	public void setStateWarn() throws Exception
+	public void handleWarning()
 	{
 		handleStates("warn");
 	}
 
 	@Expose
-	public void setStateError() throws Exception
+	public void handleError()
 	{
 		handleStates("error");
 	}
