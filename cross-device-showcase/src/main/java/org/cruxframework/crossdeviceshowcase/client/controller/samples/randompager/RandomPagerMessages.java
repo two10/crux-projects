ce761fa08ecb5f5b004e22d3717dacc1b60bf395
flavia <flavia@triggolabs.com>
2014-07-21 15:57:07 +0000

    ISSUE 22 - Add RandomPager in ShowCase

---
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/randompager/RandomPagerMessages.java
@@ -0,0 +1,144 @@
+package org.cruxframework.crossdeviceshowcase.client.controller.samples.randompager;
+
+import org.cruxframework.crux.core.client.i18n.MessageName;
+
+import com.google.gwt.i18n.client.Messages;
+
+@MessageName("messages_randomPager")
+public interface RandomPagerMessages extends Messages
+{
+	@DefaultMessage("Random Pager")
+	String title();
+	
+	@DefaultMessage("América do Sul")
+	String southAmerica();
+	
+	@DefaultMessage("America Central")
+	String centralAmerica();
+	
+	@DefaultMessage("América do Norte")
+	String america();
+	
+	@DefaultMessage("Europa")
+	String europe();
+	
+	@DefaultMessage("Ásia")
+	String asia();
+	
+	@DefaultMessage("África")
+	String africa();
+	
+	@DefaultMessage("Oceania")
+	String oceania();
+	
+	@DefaultMessage("Argentina")
+	String argentina();
+	
+	@DefaultMessage("Buenos Aires")
+	String capitalArgentina();
+	
+	@DefaultMessage("Brasil")
+	String brazil();
+	
+	@DefaultMessage("Brasil")
+	String capitalBrazil();
+	
+	@DefaultMessage("Chile")
+	String chile();
+	
+	@DefaultMessage("Chile")
+	String capitalChile();
+	
+	@DefaultMessage("Costa Rica")
+	String costaRica();
+	
+	@DefaultMessage("São José")
+	String capitalCostaRica();
+	
+	@DefaultMessage("Estados Unidos")
+	String usa();
+	
+	@DefaultMessage("Washington")
+	String capitalUsa();
+	
+	@DefaultMessage("México")
+	String mexico();
+	
+	@DefaultMessage("Cidade do México")
+	String capitalMexico();
+	
+	@DefaultMessage("Canadá")
+	String canada();
+	
+	@DefaultMessage("Ottawa")
+	String capitalCanada();
+	
+	@DefaultMessage("Alemanha")
+	String germany();
+	
+	@DefaultMessage("Berlim")
+	String capitalGermany();
+	
+	@DefaultMessage("Espanha")
+	String spain();
+	
+	@DefaultMessage("Madri")
+	String capitalSpain();
+	
+	@DefaultMessage("França")
+	String france();
+	
+	@DefaultMessage("Paris")
+	String capitalFrance();
+	
+	@DefaultMessage("Itália")
+	String italy();
+	
+	@DefaultMessage("Roma")
+	String capitalItaly();
+	
+	@DefaultMessage("China")
+	String china();
+	
+	@DefaultMessage("Pequim")
+	String capitalChina();
+
+	@DefaultMessage("Corea do Sul")
+	String southKorea();
+	
+	@DefaultMessage("Seul")
+	String capitalSouthKorea();
+
+	@DefaultMessage("Japão")
+	String japan();
+	
+	@DefaultMessage("Tóquio")
+	String capitalJapan();
+	
+	@DefaultMessage("África do Sul")
+	String southAfrica();
+	
+	@DefaultMessage("Pretória")
+	String capitalSouthAfrica();
+	
+	@DefaultMessage("Argélia")
+	String algeria();
+	
+	@DefaultMessage("Argel")
+	String capitalAlgeria();
+
+	@DefaultMessage("Marrocos")
+	String morocco();
+	
+	@DefaultMessage("Rabat")
+	String capitalMorocco();
+	
+	@DefaultMessage("Austrália‎")
+	String australia();
+	
+	@DefaultMessage("Camberra")
+	String capitalAustralia();
+
+	@DefaultMessage("<p>Random Pager.</p>")
+	String htmlDescText();
+}
