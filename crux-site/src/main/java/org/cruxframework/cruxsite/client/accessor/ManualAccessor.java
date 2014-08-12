0da37e71260321cec961bc1411d2b7276deba736
alexandre <alexandre@triggolabs.com>
2014-08-12 17:47:50 +0000

    IGNORE - creating tag 5.1.0

--- crux-site/src/main/java/org/cruxframework/cruxsite/client/accessor/ManualAccessor.java
+++
@@ -1,8 +0,0 @@
-package org.cruxframework.cruxsite.client.accessor;
-
-import org.cruxframework.crux.core.client.screen.views.WidgetAccessor;
-import org.cruxframework.crux.widgets.client.anchor.Anchor;
-
-public interface ManualAccessor extends WidgetAccessor {
-
-}
