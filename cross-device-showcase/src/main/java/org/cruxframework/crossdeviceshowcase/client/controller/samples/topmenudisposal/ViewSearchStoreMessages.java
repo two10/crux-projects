3b4589544f9f32cad1b15779eacdde80720a8fd4
juliano <juliano@cruxframework.org>
2014-08-21 14:00:17 +0000

    Issue 47-Necessary adjustments in Showcase (standardization).

--- cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/topmenudisposal/ViewSearchStoreMessages.java
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/topmenudisposal/ViewSearchStoreMessages.java
@@ -12,34 +12,7 @@ public interface ViewSearchStoreMessages extends Messages
 	
 	@DefaultMessage("Encontre uma loja mais perto de você.")
 	String labelInfo();
-	
-	@DefaultMessage("Selecione o País")
-	String labelSearch();
-	
-	@DefaultMessage("Brasil")
-	String brazil();
-	
-	@DefaultMessage("Estado Unidos")
-	String usa();
-	
-	@DefaultMessage("México")
-	String mexico();
-	
-	@DefaultMessage("Canadá")
-	String canada();
-	
-	@DefaultMessage("França")
-	String france();
-	
-	@DefaultMessage("Itália")
-	String italy();
-	
-	@DefaultMessage("Japão")
-	String japan();
-	
-	@DefaultMessage("Buscar Lojas")
-	String searchStores();
-	
+		
 	@DefaultMessage("Lojas Encontradas.")
 	String storesFound();
 }
