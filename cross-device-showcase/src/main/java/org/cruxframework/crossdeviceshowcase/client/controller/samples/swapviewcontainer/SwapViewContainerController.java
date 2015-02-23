71af0fe68226a841353dcd20ee86917c913b2dd6
flavia.jesus <flavia.jesus@triggolabs.com>
2015-02-23 14:28:06 +0000

    FIXED Issue #63 - ENHANCEMENT - : Addition of the Faces SwapViewContainer component in showCase.

--- cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/swapviewcontainer/SwapViewContainerController.java
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/swapviewcontainer/SwapViewContainerController.java
@@ -1,55 +1,142 @@
 package org.cruxframework.crossdeviceshowcase.client.controller.samples.swapviewcontainer;
 
+import org.cruxframework.crossdeviceshowcase.shared.messages.DescriptionMessages;
 import org.cruxframework.crux.core.client.controller.Controller;
 import org.cruxframework.crux.core.client.controller.Expose;
 import org.cruxframework.crux.core.client.ioc.Inject;
+import org.cruxframework.crux.core.client.screen.DeviceAdaptive.Device;
 import org.cruxframework.crux.core.client.screen.views.BindView;
-import org.cruxframework.crux.core.client.screen.views.View;
 import org.cruxframework.crux.core.client.screen.views.WidgetAccessor;
-import org.cruxframework.crux.widgets.client.swapcontainer.SwapContainer;
+import org.cruxframework.crux.smartfaces.client.button.Button;
+import org.cruxframework.crux.smartfaces.client.swappanel.SwapAnimation;
+import org.cruxframework.crux.smartfaces.client.swappanel.SwapViewContainer;
 
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ListBox;
 
 @Controller("swapViewContainerController")
 public class SwapViewContainerController 
 {	
+	int count = 1;
+	
 	@Inject
-	private MyWidgetAccessor myWidgetAccessor;
+	private DescriptionMessages messages;
 
 	@Inject
-	private SwapViewContainerMessages messages;
+	private MyWidgetAccessor myWidgetAccessor;
 
 	@Expose
 	public void onLoad()
 	{
 		/* Insert the component description*/
-		myWidgetAccessor.htmlDescText().setHTML(messages.htmlDescText());
+		myWidgetAccessor.componentDescription().setHTML(messages.sideMenuDisposal());
 	}
 	
 	@Expose
-	public void showNextView()
+	public void nextView()
 	{
-		View activeView = myWidgetAccessor.viewContainer().getActiveView();
-		
-		if (activeView != null)
+		switch (count) 
 		{
-			String activeViewId = activeView.getId();
-			
-			if (activeViewId.equals("swapView1"))
-			{
-				myWidgetAccessor.viewContainer().showView("swapView2");
-			}else
-			{
-				myWidgetAccessor.viewContainer().showView("swapView1");
-			}
+		case 1:
+			setState2();
+			break;
+		case 2:
+			setState3();
+			break;
+		case 3:
+			setState1();
+			break;	
 		}
+	}
+	
+	@Expose
+	public void previousView()
+	{
+		switch (count) 
+		{
+		case 2:
+			setState1();
+			break;
+		case 3:
+			setState2();
+			break;
+		}
+	}
+	
+	private void setState1()
+	{
+		myWidgetAccessor.viewContainer().setAnimation(SwapAnimation.fade);
+		myWidgetAccessor.viewContainer().setAnimationEnabled(true, Device.all);
+		myWidgetAccessor.viewContainer().showView("swapView1");
+		myWidgetAccessor.backButton().setVisible(false);
+		myWidgetAccessor.buyButton().setText("Buy Now");
+		count = 1;
+	}
+	
+	private void setState2()
+	{
+		myWidgetAccessor.viewContainer().setAnimation(SwapAnimation.fade);
+		myWidgetAccessor.viewContainer().setAnimationEnabled(true, Device.all);
+		myWidgetAccessor.viewContainer().showView("swapView2");
+		myWidgetAccessor.backButton().setVisible(true);
+		myWidgetAccessor.buyButton().setText("Checkout");
+		count = 2;
+	}
+	
+	private void setState3()
+	{
+		myWidgetAccessor.viewContainer().setAnimation(SwapAnimation.fade);
+		myWidgetAccessor.viewContainer().setAnimationEnabled(true, Device.all);
+		myWidgetAccessor.viewContainer().showView("swapView3");
+		myWidgetAccessor.backButton().setVisible(true);
+		myWidgetAccessor.buyButton().setText("Back to Start");
+		count = 3;
+	}
+	
+//	Returns animation type chosen in the listBox by the user
+	private SwapAnimation chooseAnimation()
+	{
+		SwapAnimation swapAnimation = null;
+		
+		String animation = myWidgetAccessor.listAnimation().getValue(myWidgetAccessor.listAnimation().getSelectedIndex());
+		
+		switch (animation) 
+		{
+		case "bounce":
+			swapAnimation = SwapAnimation.bounce;
+			break;
+		case "fade":
+			swapAnimation = SwapAnimation.fade;
+			break;	
+		case "flipX":
+			swapAnimation = SwapAnimation.flipX;
+			break;	
+		case "flipY":
+			swapAnimation = SwapAnimation.flipY;
+			break;	
+		case "lightSpeed":
+			swapAnimation = SwapAnimation.lightSpeed;
+			break;			
+		case "roll":
+			swapAnimation = SwapAnimation.roll;
+			break;	
+		case "rotate":
+			swapAnimation = SwapAnimation.rotate;
+			break;		
+		default:
+			break;
+		}
+		return swapAnimation;
 	}
 
 	@BindView("swapViewContainer")
 	public static interface MyWidgetAccessor extends WidgetAccessor
 	{
-		HTML htmlDescText();
-		SwapContainer viewContainer();
+		Button buyButton();
+		Button backButton();
+		ListBox listAnimation();
+		SwapViewContainer viewContainer();
+		HTML componentDescription();
 	}
 
 	public void setMyWidgetAccessor(MyWidgetAccessor myWidgetAccessor) 
@@ -57,7 +144,7 @@ public class SwapViewContainerController
 		this.myWidgetAccessor = myWidgetAccessor;
 	}
 
-	public void setMessages(SwapViewContainerMessages messages) 
+	public void setMessages(DescriptionMessages messages) 
 	{
 		this.messages = messages;
 	}
